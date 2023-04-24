c5fc3ff8a9f67dab56cdbd30f61684c19bd1c628
raobaba <raorajan9576@gmail.com>
2023-04-24 10:07:31 +0530

    Enough Fuel Consumption

---
+++ Masai/Enough_Fuel_Consumption.js
@@ -0,0 +1,16 @@
+let fuel = 46;
+let distance = 1;
+
+function enoughFuelConsumption(fuel, distance) {
+    //write code here
+ 
+    var a = fuel;
+    var b = distance;
+     required = (a*b);
+    if(required>50)
+    console.log("Enough");
+    else
+    console.log("Go On");
+    
+    
+}
