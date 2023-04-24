eceef0d4600283ea01e31763b404b4d92c85935a
raobaba <raorajan9576@gmail.com>
2023-04-24 10:10:56 +0530

    Print Grade

---
+++ Masai/Print_Grade.js
@@ -0,0 +1,13 @@
+let total = 80;
+function printGrade(total) {
+    // Write code here
+    var t = total;
+    if(total == 100)
+    console.log("A");
+    else if(total >= 90)
+    console.log("B");
+    else if(total >= 80)
+    console.log("C");
+    else
+    console.log("F");
+}
