41abf5c540843f5dc9125720768204a75ef446cf
raobaba <raorajan9576@gmail.com>
2023-04-24 10:03:17 +0530

    The Ashes

---
+++ Masai/The_Ashes.js
@@ -0,0 +1,16 @@
+let Australia = 46;
+let England = 67;
+function theAshes(Australia, England) {
+    //Write code here
+       var a = Australia;
+       var b = England;
+         
+         if(a>b)
+         console.log("Australia");
+          if(a<b)
+         console.log("England");
+          if(a == b)
+         console.log("Tie");
+    
+    
+    }
