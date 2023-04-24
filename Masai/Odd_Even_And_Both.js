f76d96e0765252874be1ce5c1711203168bbfff5
raobaba <raorajan9576@gmail.com>
2023-04-24 10:26:03 +0530

    week day

---
+++ Masai/Odd_Even_And_Both.js
@@ -0,0 +1,13 @@
+let one = 3;
+let two = 6;
+
+function oddEvenBoth(one, two) {
+    // Write code here
+    if((one%2===0)&&(two%2===0))
+    console.log("Even");
+    else if((one%2==1)&&(two%2==1))
+    console.log("Odd");
+    else 
+    console.log("Even-Odd");
+    
+}
---
+++ Masai/Week_Day.js
@@ -0,0 +1,20 @@
+let num = 2;
+function weekday(num) {
+    // Write code here
+    var a = num;
+    
+    if(a==1)
+    console.log("Monday");
+    if(a==2)
+    console.log("Tuesday");
+    if(a==3)
+    console.log("Wednesday");
+    if(a==4)
+    console.log("Thursday");
+    if(a==5)
+    console.log("Friday");
+    if(a==6)
+    console.log("Saturday");
+    if(a==7)
+    console.log("Sunday");
+}
