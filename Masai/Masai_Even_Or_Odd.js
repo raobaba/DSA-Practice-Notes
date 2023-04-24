f0658af0af46802c4b7bbe973d8fee3074a26468
raobaba <raorajan9576@gmail.com>
2023-04-24 10:04:32 +0530

    Masai Even Or Odd

---
+++ Masai/Masai_Even_Or_Odd.js
@@ -0,0 +1,14 @@
+let N = 5;
+function masaiEvenOrOdd(N) {
+    // Write code here
+    var number = N;
+    
+    if(N%2==0)
+    {
+        console.log("Even");
+    }
+    else
+    {
+        console.log("Odd");
+    }
+}
