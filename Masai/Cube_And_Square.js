e9fb627840cabf8e2105cb933411810f87ffe0c1
raobaba <raorajan9576@gmail.com>
2023-04-24 10:00:34 +0530

    cube and square

---
+++ Masai/Cube_And_Square.js
@@ -0,0 +1,16 @@
+let n = 3;
+let m = 2;
+
+function cubeAndSquare(n,m) {
+    //write code here
+    var cube_of_n = n**3;
+    var square_of_m = m**2;
+    if(cube_of_n>square_of_m)
+    {
+        console.log("true");
+    }
+    else
+    {
+        console.log("false");
+    }
+}
