a5f04d74d0afe92d49ca6aeafb03903104486c26
raobaba <raorajan9576@gmail.com>
2023-04-24 10:21:55 +0530

    Logical Events

---
+++ Masai/Logical_Events.js
@@ -0,0 +1,16 @@
+let one = 4;
+let two = 7;
+function logicalEvens(one, two) {
+    // Write code here
+    var a = one;
+    var b = two;
+    
+    if((a%2===0)&&(b%2===0))
+    {
+        console.log("Both");
+    }
+    else
+    {
+        console.log("No");
+    }
+}
