93f814a86b9824d571db17b119108db48033f843
raobaba <raorajan9576@gmail.com>
2023-04-24 10:20:30 +0530

    compare two sum

---
+++ Masai/Compare_Two_Sum.js
@@ -0,0 +1,20 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+function twoSumsOneNumberII(one, two, three, four, five, six) {
+    // Write code here
+    sum1 = one+two;
+    sum2 = four+five;
+    
+    if((sum1>three)&&(sum2>six))
+    {
+        console.log("Yes");
+    }
+    else
+    {
+        console.log("No");
+    }
+}
