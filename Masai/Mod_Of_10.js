3d60acbe29947fb98290e57f9fae475296a2a623
raobaba <raorajan9576@gmail.com>
2023-04-24 10:27:08 +0530

    mdoe of 10

---
+++ Masai/Mod_Of_10.js
@@ -0,0 +1,27 @@
+let num = 12;
+modeof10(num);
+function modeof10(num) {
+    // Write code here
+    var x = num%10;
+    
+     if(x===0)
+     console.log("zero");
+     if(x==1)
+     console.log("one");
+     if(x==2)
+     console.log("two");
+     if(x==3)
+     console.log("three");
+     if(x==4)
+     console.log("four");
+     if(x==5)
+     console.log("five")
+     if(x==6)
+     console.log("six");
+     if(x==7)
+     console.log("seven");
+     if(x==8)
+     console.log("eight");
+     if(x==9)
+     console.log("nine");
+}
