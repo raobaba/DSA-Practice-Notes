70ca6024b007a8e1bccf4c8e79b89e0026c2aaaf
raobaba <raorajan9576@gmail.com>
2023-04-24 10:16:46 +0530

    Compare four number

---
+++ Masai/Compare_Four_Number.js
@@ -0,0 +1,21 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+function compareFourNumbers(one, two, three, four) {
+    // Write code here
+    var a = one;
+    var b = two;
+    var c = three;
+    var d = four;
+    
+    if((one>two)&&(three>four))
+    {
+        console.log("Yes");
+    }
+    else
+    {
+        console.log("No");
+    }
+    
+}
