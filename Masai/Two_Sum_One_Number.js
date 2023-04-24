ac3439fc57745eaa33156c329c422d090901e5b4
raobaba <raorajan9576@gmail.com>
2023-04-24 10:18:37 +0530

    two sum one number

---
+++ Masai/Two_Sum_One_Number.js
@@ -0,0 +1,19 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+
+function twoSumsOneNumberII(one, two, three, four, five) {
+    // Write code here
+    sum1 = one+two;
+    sum2 = three+four;
+    if((sum1>five)||(sum2>five))
+    {
+        console.log("Yes");
+    }
+    else
+    {
+        console.log("No")
+    }
+}
