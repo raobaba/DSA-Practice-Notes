2bcff23a6e9b4b7f39687bcc58ba149888f84023
raobaba <raorajan9576@gmail.com>
2023-04-24 10:05:51 +0530

    Apply Breaks

---
+++ Masai/Apply_Break.js
@@ -0,0 +1,14 @@
+let distance = 100;
+let time = 2;
+function applyBrakes(distance, time) {
+    //Write code here
+     var d = distance;
+     var t = time;
+    speed = (d/t);
+    
+    if(speed>40)
+    console.log("Apply Brake");
+    else
+    console.log("Keep Going");
+    
+}
