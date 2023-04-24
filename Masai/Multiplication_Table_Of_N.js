88b7de0b60e23a9f11f70c31d6b546b15f5d3a2b
raobaba <raorajan9576@gmail.com>
2023-04-24 10:29:41 +0530

    Multiplication table of n

---
+++ Masai/Multiplication_Table_Of_N.js
@@ -0,0 +1,14 @@
+let num = 4;
+multiplicationTableN(num);
+
+function multiplicationTableN(num) {
+    // Write code here
+    var i = 1;
+    while(i<=10)
+    {
+        console.log(i*num);
+        i++;
+    }
+    
+    
+}
