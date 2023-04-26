0ce6080db5adf2ff47e57103b516b3ac92a71468
raobaba <raorajan9576@gmail.com>
2023-04-26 20:40:37 +0530

    Check Greater

---
+++ Masai/Check_Greate.js
@@ -0,0 +1,16 @@
+var [A,B,C,D] = [26,20,30,28];
+checkGreater(A,B,C,D);
+
+function checkGreater(A, B, C, D) {
+  //write code here
+  
+  
+  if(A>B && C>D)
+  {
+      console.log("true");
+  }
+  else 
+  {
+      console.log("false");
+  }
+}
