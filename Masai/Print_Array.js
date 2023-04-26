a43da0ce41e60d360410ea7d744d0f125fc5126f
raobaba <raorajan9576@gmail.com>
2023-04-26 22:10:09 +0530

    Print Array

---
+++ Masai/Print_Array.js
@@ -0,0 +1,12 @@
+let arr = [1,2,3,4,5];
+let N = 5;
+printArray(N,arr);
+
+function printArray(N,arr){
+    //write code here
+     
+     for(var i=0;i<=N-1;i++)
+     {
+         console.log(arr[i]);
+     }
+}
