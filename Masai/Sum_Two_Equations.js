d4c54a2649372fc2e53928359a2325254377fd2a
raobaba <raorajan9576@gmail.com>
2023-04-26 20:43:15 +0530

    Sum Two Equations

---
+++ Masai/Sum_Two_Equations.js
@@ -0,0 +1,11 @@
+let [a,b] = [2,3];
+sumTwoEqs(a,b);
+function sumTwoEqs(a, b) {
+    // write code here
+    var n = a;
+    var m = b;
+    var X = (n*5) + (m*3);
+    var Y = (n*7) + (m*4);
+    console.log(X+Y);
+    
+  }
