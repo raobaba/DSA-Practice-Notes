fbf21c14268029101029a1a588e153ec98d8bfe0
raobaba <raorajan9576@gmail.com>
2023-04-26 22:16:52 +0530

    Print Character

---
+++ Masai/Print_Character.js
@@ -0,0 +1,13 @@
+let str = "Rajan";
+let N = 5;
+printChar(N,str);
+function printChar(N,str) {
+    //write code here
+    var size = str.length;
+    for(var i=0;i<=N-1;i++)
+    {
+        console.log(str[i]);
+    }
+    
+
+}
