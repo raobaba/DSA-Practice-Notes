02a47c5a7ebbe925ef72a33843200a62ca4bfd14
raobaba <raorajan9576@gmail.com>
2023-04-27 16:54:18 +0530

    Transepose the matrix

---
+++ Masai/Transepose_The_matrix.js
@@ -0,0 +1,17 @@
+let N = 5;
+let M = 4;
+let matrix = [[0,0,0,0,0],[1,1,1,1,1],[2,2,2,2,2],[3,3,3,3,3]];
+transposeTheMatrix(N, M, matrix)
+function transposeTheMatrix(N, M, matrix){
+    //write code here
+    for(var i=0;i<M;i++)
+    {
+        var bag = "";
+        for(var j=0;j<N;j++)
+        {
+            bag = bag + matrix[j][i]+" ";
+        }
+        console.log(bag);
+    }
+  
+}
