f052d4bd01b6697d906542374244bef4e17dcabe
raobaba <raorajan9576@gmail.com>
2023-06-08 16:21:25 +0530

    Merge Sorted Array

---
+++ Arrays/Merge_Sorted_Array.js
@@ -0,0 +1,31 @@
+let nums1 = [1,2,3,0,0,0];
+let m = nums1.length-1;
+let nums2 = [4,5,6];
+let n = nums2.length-1;
+
+let res = MergeSortedArray(nums1,m,nums2,n);
+console.log(res);
+function MergeSortedArray(nums1,m,nums2,n){
+    let p1 = m - 1;
+    let p2 = n - 1;
+    let p = m + n - 1;
+  
+    while (p1 >= 0 && p2 >= 0) {
+      if (nums1[p1] > nums2[p2]) {
+        nums1[p] = nums1[p1];
+        p1--;
+      } else {
+        nums1[p] = nums2[p2];
+        p2--;
+      }
+      p--;
+    }
+  
+    while (p2 >= 0) {
+      nums1[p] = nums2[p2];
+      p2--;
+      p--;
+    }
+  
+    return nums1;
+}
