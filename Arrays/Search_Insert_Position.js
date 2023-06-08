eb976d3d54f466c008a5253f8a48bf98ff1b5f3e
raobaba <raorajan9576@gmail.com>
2023-06-08 15:33:59 +0530

    search insert

---
+++ Arrays/Search_Insert_Position.js
@@ -0,0 +1,19 @@
+let arr = [1,2,3,5,6];
+let target = 4;
+let res = SearchInsert(arr,target);
+function SearchInsert(nums,target){
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+      let middle = Math.floor(left + (right - left) / 2);
+      if (nums[middle] === target) {
+        return middle;
+      } else if (nums[middle] < target) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
+    }
+  
+    return left;
+}
